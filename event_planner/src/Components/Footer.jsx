05f028ae1b30ae8d8d3c2eed31355398e5146305
Towhid Raihan <163158733+towhidxraihan@users.noreply.github.com>
2025-02-27 17:05:19 +0000

    Add files via upload

---
+++ event_planner/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+      import React from 'react';
+     const Footer = () => {
+         return (
+             <>
+      <footer>
+      <p>&copy; Event Planner Organization. All rights reserved. Recreated by Towhid Raihan</p>
+      </footer>
+             </>
+         )}
+     export default Footer
